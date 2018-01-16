79a1ee0a583bbc48beee68fceb7b15629b1bb2f9
defied <d.rhys-jones@temperednetworks.com>
2018-01-16 15:00:34 -0800

    Updated serial connect method. Created 'Version' output.

--- robolidar/lidar-manager.py
+++ robolidar/lidar-manager.py
@@ -2,7 +2,7 @@
 __author__ = 'Dafydd'
 #
 import argparse
-from lib import serial_control
+import serial
 import logging
 import time
 import sys
@@ -15,8 +15,8 @@ host = '10.25.57.2'
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--port', metavar='', help='set serial device port', default='/dev/serial')
 parser.add_argument('-b', '--baud', metavar='', type=int, help='set baudrate', default=115200)
-parser.add_argument('-c', '--commandn', metavar='', help='send custom command')
-parser.add_argument('-v', '--version', metavar='', help='show version')
+parser.add_argument('-c', '--command', metavar='', help='send custom command')
+parser.add_argument('-v', '--version', action="store_true", default=False)
 parser.add_argument('-ms', '--set_motor', metavar='', type=int, help='adjust Motor Speed [1-10]', choices=list(range(1,11)), default=5)
 parser.add_argument('-lr', '--set_lidar', metavar='', type=int, help='adjust LiDAR Sample Rate [1,2,3]', choices=list(range(1,4)), default=2)
 args = parser.parse_args()
@@ -33,27 +33,34 @@ logging.debug('Beginning Logging.')
 statLog = logging
 
 # Network tables
-NetworkTables.initialize(server=host)
+# NetworkTables.initialize(server=host)
 
 
 # lidar
 def serial_connect(console_port, baud_rate):
     ''' Calls the Serial controller and creates the connection.'''
     try:
-        ser = serial_control.Serialcontrol()
-        ser.connect(console_port, baud_rate)
+        ser = serial.Serial(console_port, baud_rate, parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE, xonxoff=False, rtscts=False, dsrdtr=False)
     except:
         ser = 'Something broke in Serial.'
     return ser
 
 def display_info():
     ''' Print IV, LIm MI, IV, ID.'''
-    info={}
-    info[version] = 'Version: {}'.format(lidar.send('IV','')[1])
-    info[motor] = 'Motor: {}'.format(lidar.send('MI','')[1])
-    info[lidar] = 'Lidar: {}'.format(lidar.send('LI','')[1])
-    info[device] = 'Device: {}'.format(lidar.send('ID','')[1])
-    return info
+    results = {}
+    lidar.write(b"IV\r\n")
+    time.sleep(1)
+    results['Version'] = lidar.read(lidar.inWaiting()).decode('utf-8')
+    lidar.write(b'LI\r\n')
+    time.sleep(1)
+    results['Resolution'] = lidar.read(lidar.inWaiting()).decode('utf-8')
+    lidar.write(b'MI\r\n')
+    time.sleep(1)
+    results['Speed'] = lidar.read(lidar.inWaiting()).decode('utf-8')
+    lidar.write(b'ID\r\n')
+    time.sleep(1)
+    results['ID'] = lidar.read(lidar.inWaiting()).decode('utf-8')
+    return results
 
 def set_motor_speed(motor_speed):
     ''' Set the motor speed.'''
@@ -70,13 +77,8 @@ def get_lidar_ready():
     return
 
 # Connect to lidar:
+print('Connecting to lidar. Setting: {} {}\r\n'.format(args.baud, args.port))
 lidar = serial_connect(args.port, args.baud)
-
-# Validate connection:
-if 'broke' in lidar:
-    print('Failed to connect to lidar.\nReason: {}'.format(lidar))
-    statLog.debug('Failed to connect to lidar: {}'.format(lidar))
-    sys.exit(165)
 
 if args.command:
     ret = lidar.send('{}'.format(args.command),'')[1]
@@ -86,7 +88,9 @@ if args.command:
 if args.version:
     info = display_info()
     for i in info:
-        print(i)
+        m = info[i]
+        print(i + ": " + m)
+    sys.exit(0)
 
 if args.set_motor:
     set_motor_speed(args.set_motor)
